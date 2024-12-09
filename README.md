61f6aeea233b4bad3dcae7903937ce078921e940
mdop297 <lenhatminh297@gmail.com>
2024-12-09 15:03:55 +0700

    update README

---
+++ README.md
@@ -0,0 +1,1 @@
+## Machine Learning Project For Network Security Using Phishing Data
--- template.py
+++ template.py
@@ -26,6 +26,7 @@ list_of_files=[
     "params.yaml",
     "schema.py",
     "main.py",
+    "README.md",
     "Dockerfile",
     "requirements.txt",
     "setup.py",
